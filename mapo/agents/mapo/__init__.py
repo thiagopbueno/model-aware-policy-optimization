427ab6216bd1b40d17dcd344d08bc772cd2880f7
Ângelo Lovatto <angelolovatto@gmail.com>
2019-07-16 19:53:10 -0300

    chore: update MAPOTrainer config and name
    * remove deprecated "Agent" prefix
    * set default number of paraller workers to 0 (only the
      local policy of the `SyncSamplesOptimizer` will be used)
    
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- mapo/agents/mapo/__init__.py
+++ mapo/agents/mapo/__init__.py
@@ -1,7 +1,4 @@
 # pylint: disable=missing-docstring
-from ray.rllib.utils import renamed_agent
 from mapo.agents.mapo.mapo import MAPOTrainer, DEFAULT_CONFIG
 
-MAPOAgent = renamed_agent(MAPOTrainer)
-
-__all__ = ["MAPOTrainer", "MAPOAgent", "DEFAULT_CONFIG"]
+__all__ = ["MAPOTrainer", "DEFAULT_CONFIG"]
