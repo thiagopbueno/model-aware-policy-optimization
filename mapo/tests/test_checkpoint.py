02e9f012f27edc19baf53342c4d5aef2c5454b7b
Ângelo Lovatto <angelolovatto@gmail.com>
2019-07-25 17:30:03 -0300

    test: parametrize tests to check all algorithms
    Some obscure warnings appear in the output of pytest, but
    nothing that seems to indicate an error.
    
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- mapo/tests/test_checkpoint.py
+++ mapo/tests/test_checkpoint.py
@@ -1,21 +1,24 @@
 """Tests for agent saving and loading."""
+import pytest
 import numpy as np
 import ray
 from ray.tune import register_env
 from mapo.tests.mock_env import MockEnv
-from mapo.agents.mapo import MAPOTrainer
+from mapo.agents.registry import ALGORITHMS
 
 
-def test_checkpoint_restore(tmpdir):
+@pytest.mark.parametrize("get_trainer", list(ALGORITHMS.values()))
+def test_checkpoint_restore(tmpdir, get_trainer):
     """
     Check if agents can be succesfully restored.
 
     Assumes the result of `compute_action` is deterministic for a given observation.
     """
-    ray.init()
+    ray.init(ignore_reinit_error=True)
     register_env("test", lambda _: MockEnv())
-    agent1 = MAPOTrainer(env="test")
-    agent2 = MAPOTrainer(env="test")
+    trainer = get_trainer()
+    agent1 = trainer(env="test")
+    agent2 = trainer(env="test")
 
     for _ in range(3):
         agent1.train()
