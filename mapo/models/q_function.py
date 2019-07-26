4d2b8980988ce97fed8932949c32373c0f77b761
Ângelo Lovatto <angelolovatto@gmail.com>
2019-07-26 14:31:53 -0300

    fix: correct default model configurations
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- mapo/models/q_function.py
+++ mapo/models/q_function.py
@@ -4,7 +4,7 @@ from tensorflow import keras
 DEFAULT_CONFIG = {"hidden_activation": "relu", "hidden_units": [64, 64]}
 
 
-def build_continuous_q_function(obs_space, action_space, config):
+def build_continuous_q_function(obs_space, action_space, config=None):
     """
     Construct continuous Q function keras model.
 
@@ -15,10 +15,10 @@ def build_continuous_q_function(obs_space, action_space, config):
 
     obs_input = keras.Input(shape=obs_space.shape)
     action_input = keras.Input(shape=action_space.shape)
-    activation = config["critic_hidden_activation"]
+    activation = config["hidden_activation"]
 
     output = keras.layers.concatenate([obs_input, action_input])
-    for hidden in config["critic_hiddens"]:
+    for hidden in config["hidden_units"]:
         output = keras.layers.Dense(units=hidden, activation=activation)(output)
     output = keras.layers.Dense(units=1, activation=None)(output)
     return keras.Model(inputs=[obs_input, action_input], outputs=output)
