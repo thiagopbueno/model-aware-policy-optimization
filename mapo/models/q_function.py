e90cd932bafd154b3e67c63aa9a043b70e0253b4
Ângelo Lovatto <angelolovatto@gmail.com>
2019-08-01 09:52:16 -0300

    feat: add fully connected network model
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- mapo/models/q_function.py
+++ mapo/models/q_function.py
@@ -1,7 +1,6 @@
 """Utilities for constructing Q function approximators."""
 from tensorflow import keras
-
-DEFAULT_CONFIG = {"hidden_activation": "relu", "hidden_units": [400, 300]}
+from mapo.models.fcnet import build_fcnet
 
 
 def build_continuous_q_function(obs_space, action_space, config=None):
@@ -10,14 +9,10 @@ def build_continuous_q_function(obs_space, action_space, config=None):
 
     Assumes both obs_space and action_space are gym.spaces.Box instances.
     """
-    config = config or {}
-    config = {**DEFAULT_CONFIG, **config}
-
     obs_input = keras.Input(shape=obs_space.shape)
     action_input = keras.Input(shape=action_space.shape)
     output = keras.layers.Concatenate(axis=1)([obs_input, action_input])
-    activation = config["hidden_activation"]
-    for units in config["hidden_units"]:
-        output = keras.layers.Dense(units=units, activation=activation)(output)
+
+    output = build_fcnet(output.shape, config=config)(output)
     output = keras.layers.Dense(units=1, activation=None)(output)
     return keras.Model(inputs=[obs_input, action_input], outputs=output)
