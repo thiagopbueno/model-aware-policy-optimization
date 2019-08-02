213c25af756c15457695b45cfeeae2cacaff70e0
Ângelo Lovatto <angelolovatto@gmail.com>
2019-08-02 14:40:13 -0300

    refactor: remove test function docstrings
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- mapo/models/tests/test_policy.py
+++ mapo/models/tests/test_policy.py
@@ -1,4 +1,5 @@
 """Tests for policy keras model."""
+# pylint: disable=missing-docstring
 import tensorflow as tf
 from tensorflow import keras
 from mapo.tests.mock_env import MockEnv
@@ -6,8 +7,7 @@ from mapo.models.policy import build_deterministic_policy
 from mapo.models.layers import ActionSquashingLayer
 
 
-def test_shape():
-    """Check if policy output has a consistent shape."""
+def test_policy_output_has_consitent_shape():
     env = MockEnv()
     ob_space, ac_space = env.observation_space, env.action_space
     policy_model = build_deterministic_policy(ob_space, ac_space)
@@ -16,8 +16,7 @@ def test_shape():
     assert output.shape.as_list() == tf.TensorShape((None,) + ac_space.shape).as_list()
 
 
-def test_is_serializable():
-    """Check if policy can export config and be recreated from it."""
+def test_policy_model_is_serializable():
     env = MockEnv()
     ob_space, ac_space = env.observation_space, env.action_space
     policy_model = build_deterministic_policy(ob_space, ac_space)
