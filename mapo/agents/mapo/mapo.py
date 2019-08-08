3dafe868aa32dc8e7298a4addc44cf66cbe2f216
Thiago P. Bueno <thiago.pbueno@gmail.com>
2019-08-08 15:12:42 -0300

    Revert "refactor: use keras functional API for models"
    This reverts commit f6d3eabf2b36ca106ad3fac09180b2ea3c2793d3.

--- mapo/agents/mapo/mapo.py
+++ mapo/agents/mapo/mapo.py
@@ -12,17 +12,31 @@ logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 DEFAULT_CONFIG = with_common_config(
     {
         # === Model ===
-        # Process the observation input tensors with these hidden layers.
+        # Apply a state preprocessor with spec given by the "model" config option
+        # (like other RL algorithms). This is mostly useful if you have a weird
+        # observation shape, like an image. Disabled by default.
+        "use_state_preprocessor": False,
+        # Postprocess the policy network model output with these hidden layers. If
+        # use_state_preprocessor is False, then these will be the *only* hidden
+        # layers in the network.
         "actor_hiddens": [64, 64],
-        # Hidden layers activation of the policy network
+        # Hidden layers activation of the postprocessing stage of the policy
+        # network
         "actor_hidden_activation": "relu",
-        # Process the observation and action input tensors with these hidden layers.
+        # Postprocess the critic network model output with these hidden layers;
+        # again, if use_state_preprocessor is True, then the state will be
+        # preprocessed by the model specified with the "model" config option first.
         "critic_hiddens": [64, 64],
-        # Hidden layers activation of the critic.
+        # Hidden layers activation of the postprocessing state of the critic.
         "critic_hidden_activation": "relu",
         # === Resources ===
         # Number of actors used for parallelism
         "num_workers": 0,
+        # === Optimization ===
+        # Learning rate for the critic (Q-function) optimizer.
+        "critic_lr": 1e-3,
+        # Learning rate for the actor (policy) optimizer.
+        "actor_lr": 1e-3,
     }
 )
 
