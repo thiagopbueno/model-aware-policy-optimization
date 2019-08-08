82cb0425cf9f5c53e700c909d95175080514c095
Thiago P. Bueno <thiago.pbueno@gmail.com>
2019-08-08 15:12:42 -0300

    Revert "chore: add agents module skeleton"
    This reverts commit 09bbd147637306790f6ffe9afe9124ccb5f925f5.

--- mapo/agents/mapo/__init__.py
+++
@@ -1,7 +0,0 @@
-# pylint: disable=missing-docstring
-from ray.rllib.utils import renamed_agent
-from mapo.agents.mapo.mapo import MAPOTrainer, DEFAULT_CONFIG
-
-MAPOAgent = renamed_agent(MAPOTrainer)
-
-__all__ = ["MAPOTrainer", "MAPOAgent", "DEFAULT_CONFIG"]
