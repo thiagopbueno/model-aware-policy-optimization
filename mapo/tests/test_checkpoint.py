7b80d42c926c125d92546e7ab0e312d81a7f446c
Thiago P. Bueno <thiago.pbueno@gmail.com>
2019-08-08 15:12:42 -0300

    Revert "test: parametrize tests to check all algorithms"
    This reverts commit 02e9f012f27edc19baf53342c4d5aef2c5454b7b.

--- mapo/tests/test_checkpoint.py
+++ mapo/tests/test_checkpoint.py
@@ -1,24 +1,21 @@
 """Tests for agent saving and loading."""
-import pytest
 import numpy as np
 import ray
 from ray.tune import register_env
 from mapo.tests.mock_env import MockEnv
-from mapo.agents.registry import ALGORITHMS
+from mapo.agents.mapo import MAPOTrainer
 
 
-@pytest.mark.parametrize("get_trainer", list(ALGORITHMS.values()))
-def test_checkpoint_restore(tmpdir, get_trainer):
+def test_checkpoint_restore(tmpdir):
     """
     Check if agents can be succesfully restored.
 
     Assumes the result of `compute_action` is deterministic for a given observation.
     """
-    ray.init(ignore_reinit_error=True)
+    ray.init()
     register_env("test", lambda _: MockEnv())
-    trainer = get_trainer()
-    agent1 = trainer(env="test")
-    agent2 = trainer(env="test")
+    agent1 = MAPOTrainer(env="test")
+    agent2 = MAPOTrainer(env="test")
 
     for _ in range(3):
         agent1.train()
--- mapo/tests/test_rollout.py
+++ mapo/tests/test_rollout.py
@@ -1,17 +1,12 @@
 """Tests for agent-environment interface."""
-import pytest
-
 from mapo.tests.mock_env import MockEnv
-from mapo.agents.registry import ALGORITHMS
+from mapo.agents.mapo.mapo_policy import MAPOTFPolicy
 
 
-@pytest.mark.parametrize("get_trainer", list(ALGORITHMS.values()))
-def test_compute_single_action(get_trainer):
+def test_compute_single_action():
     """Test if policy returns single action from the correct space."""
     env = MockEnv()
-    trainer = get_trainer()
-    # pylint: disable=protected-access
-    policy = trainer._policy(env.observation_space, env.action_space, {})
+    policy = MAPOTFPolicy(env.observation_space, env.action_space, {})
 
     obs = env.reset()
     # the list is a placeholder for RNN state inputs
