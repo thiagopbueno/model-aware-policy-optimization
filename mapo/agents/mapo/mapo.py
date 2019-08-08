721b4aa4cc67597e307432690d2f9d506ba5df0e
Thiago P. Bueno <thiago.pbueno@gmail.com>
2019-08-08 15:12:42 -0300

    Revert "fix: correct default model configurations"
    This reverts commit cbbab9deeb8873abd1ea2d12b4dafe2072e6e720.

--- mapo/agents/mapo/mapo.py
+++ mapo/agents/mapo/mapo.py
@@ -5,8 +5,6 @@ import logging
 from ray.rllib.agents.trainer import with_common_config
 from ray.rllib.agents.trainer_template import build_trainer
 from mapo.agents.mapo.mapo_policy import MAPOTFPolicy
-from mapo.models.q_function import DEFAULT_CONFIG as ACTOR_MODEL_CONFIG
-from mapo.models.policy import DEFAULT_CONFIG as CRITIC_MODEL_CONFIG
 
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
@@ -14,8 +12,14 @@ logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 DEFAULT_CONFIG = with_common_config(
     {
         # === Model ===
-        "actor_model": ACTOR_MODEL_CONFIG,
-        "critic_model": CRITIC_MODEL_CONFIG,
+        # Process the observation input tensors with these hidden layers.
+        "actor_hiddens": [64, 64],
+        # Hidden layers activation of the policy network
+        "actor_hidden_activation": "relu",
+        # Process the observation and action input tensors with these hidden layers.
+        "critic_hiddens": [64, 64],
+        # Hidden layers activation of the critic.
+        "critic_hidden_activation": "relu",
         # === Optimization ===
         # Learning rate for the critic (Q-function) optimizer.
         "critic_lr": 1e-3,
