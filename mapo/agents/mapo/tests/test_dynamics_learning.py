37e94a81df6c7f4013b26d31e14aac8acdccbc50
Ângelo Lovatto <angelolovatto@gmail.com>
2019-08-15 07:56:34 -0300

    test: parametrize more tests
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- mapo/agents/mapo/tests/test_dynamics_learning.py
+++ mapo/agents/mapo/tests/test_dynamics_learning.py
@@ -18,43 +18,46 @@ from mapo.agents.mapo.off_mapo_policy import (
 
 
 def get_env_makers():
-    return [lambda _: MockEnv({"fixed_state": True})]
+    return [
+        lambda _: MockEnv({"fixed_state": True}),
+        lambda _: MockEnv({"fixed_state": False}),
+    ]
 
 
 def get_policies_and_configs():
     return [(MAPOTFPolicy, onpolicy_config()), (OffMAPOTFPolicy, offpolicy_config())]
 
 
-def get_config_mods():
+def get_config_overrides():
     return [
-        lambda config: merge_dicts(
-            config,
-            {
-                "model": {"custom_options": {"dynamics": {"layers": []}}},
-                "model_loss": "mle",
-            },
-        )
+        {
+            "model": {"custom_options": {"dynamics": {"layers": [10]}}},
+            "model_loss": "mle",
+        }
     ]
 
 
-def get_worker_arguments():
+def get_worker_kwargs():
     all_combinations = product(
-        get_env_makers(), get_policies_and_configs(), get_config_mods()
+        get_env_makers(), get_policies_and_configs(), get_config_overrides()
     )
     return [
-        (env, policy, config_fn(conf))
-        for env, (policy, conf), config_fn in all_combinations
+        dict(
+            env_creator=env_fn,
+            policy=policy_cls,
+            policy_config=merge_dicts(config, overrides),
+        )
+        for env_fn, (policy_cls, config), overrides in all_combinations
     ]
 
 
-@pytest.mark.parametrize("worker_args", get_worker_arguments())
-def test_model_learns_deterministic_state(worker_args):
-    env_creator, policy_cls, policy_config = worker_args
-    worker = RolloutWorker(env_creator, policy_cls, policy_config=policy_config)
+@pytest.mark.parametrize("worker_kwargs", get_worker_kwargs())
+def test_model_learns_deterministic_state(worker_kwargs):
+    worker = RolloutWorker(**worker_kwargs)
     policy = worker.get_policy()
 
     info = policy.learn_on_batch(worker.sample())
     initial_dynamics_loss = info[LEARNER_STATS_KEY]["dynamics_loss"]
-    for _ in range(10):
+    for _ in range(30):
         new_info = policy.learn_on_batch(worker.sample())
     assert new_info[LEARNER_STATS_KEY]["dynamics_loss"] <= initial_dynamics_loss
