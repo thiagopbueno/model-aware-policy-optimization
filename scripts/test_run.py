b2a996f6bffcf8ccdab47b49e643bed40db90249
Thiago P. Bueno <thiago.pbueno@gmail.com>
2019-08-20 15:08:10 -0300

    refactor(script): update scripts/test_run
    Signed-off-by: Thiago P. Bueno <thiago.pbueno@gmail.com>

--- scripts/test_run.py
+++ scripts/test_run.py
@@ -1,23 +1,55 @@
-"""Test script to check if on-policy trajectories are collected by MAPO."""
-import ray
-from ray import tune
-import mapo
+"""Test script to check if MAPO successfully learns in environments."""
+
+import numpy as np
 
 
 def main():
-    """Run MAPO in several environments with continuous action spaces."""
+    """Run MAPO."""
+    import ray
+    from ray import tune
+    import mapo
+
     mapo.register_all_agents()
+    mapo.register_all_environments()
 
     ray.init()
     tune.run(
         "MAPO",
-        stop={"timesteps_total": 1000},
+        stop={"timesteps_total": int(2e5)},
+        num_samples=20,
         config={
-            "env": tune.grid_search(
-                ["Pendulum-v0", "LunarLanderContinuous-v2", "HalfCheetah-v3"]
-            ),
+            # === COMMON CONFIG ===
+            "env": "Navigation-v0",
+            "horizon": 20,
             "num_workers": 0,
+            "sample_batch_size": 1,
+            "batch_mode": "complete_episodes",
+            "num_envs_per_worker": 8,
+            "train_batch_size": 5000,
+            "optimizer": {"num_sgd_iter": 80},
+            "observation_filter": "NoFilter",
+            # "seed": 123,
+            # === MAPO CONFIG ===
+            "branching_factor": 10,
+            "use_true_dynamics": True,
+            "model_loss": "mle",
+            "model": {
+                "custom_options": {
+                    "actor": {"activation": "elu", "layers": [32]},
+                    "critic": {"activation": "elu", "layers": [32]},
+                    "dynamics": {"activation": "elu", "layers": [32]},
+                }
+            },
+            "dynamics_lr": tune.function(
+                lambda: np.random.uniform(low=1e-3, high=1e-2)
+            ),
+            "critic_lr": tune.function(lambda: np.random.uniform(low=1e-3, high=1e-2)),
+            "actor_lr": tune.function(lambda: np.random.uniform(low=1e-3, high=1e-2)),
+            "actor_delay": 80,
+            "critic_delay": 1,
         },
+        checkpoint_freq=20,
+        checkpoint_at_end=True,
     )
 
 
