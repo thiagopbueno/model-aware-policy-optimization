d3267518cb419ddad4a2b95fd2d9d441595e3523
Ângelo Lovatto <angelolovatto@gmail.com>
2019-08-21 09:25:45 -0300

    feat: add kernel initializer option for models
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- mapo/models/q_function.py
+++ mapo/models/q_function.py
@@ -1,6 +1,5 @@
 """Utilities for constructing Q function approximators."""
 from tensorflow import keras
-from ray.rllib.models.tf.misc import normc_initializer
 
 from mapo.models import obs_input, action_input
 from mapo.models.fcnet import build_fcnet
@@ -11,14 +10,13 @@ def build_continuous_q_function(obs_space, action_space, config=None):
     Construct continuous Q function keras model.
 
     Assumes both obs_space and action_space are gym.spaces.Box instances.
-    Model is called on placeholder inputs so variables are created.
+    Model is called on placeholder inputs so that variables are created.
     """
     config = config or {}
+    fc_config = dict(config, output_layer=1)
     obs = obs_input(obs_space)
     actions = action_input(action_space)
-    output = keras.layers.Concatenate(axis=-1)([obs, actions])
-    output = build_fcnet(config)(output)
-    values = keras.layers.Dense(
-        units=1, activation=None, kernel_initializer=normc_initializer(0.01)
-    )(output)
-    return keras.Model(inputs=[obs, actions], outputs=values)
+    inputs = keras.layers.Concatenate(axis=-1)([obs, actions])
+    q_model = build_fcnet(fc_config)
+    values = q_model(inputs)
+    return keras.Model([obs, actions], values)
