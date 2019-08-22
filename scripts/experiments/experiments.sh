b74004d02a8b2a8c7b8e807ee5440b56034b49fc
Thiago P. Bueno <thiago.pbueno@gmail.com>
2019-08-22 10:43:07 -0300

    feat(script): add debug config with output=logdir if --debug else None
    Signed-off-by: Thiago P. Bueno <thiago.pbueno@gmail.com>

--- scripts/experiments/experiments.sh
+++ scripts/experiments/experiments.sh
@@ -1,14 +1,17 @@
-# nav0-baseline-td3-fcnet-1024
-mapo --run OurTD3 --env Navigation-v0 --config-actor-net actor-1024-relu.json --config-critic-net critic-1024-relu.json --actor-lr 1e-4 --critic-lr 1e-4 --sample-batch-size 1 --train-batch-size 2048 --num-samples 5 --name nav0-baseline-td3-fcnet-1024
+# nav0-td3-baseline-fcnet-64
+mapo --run OurTD3 --env Navigation-v0 --config-actor-net fcnet-64-elu.json --config-critic-net fcnet-64-elu.json --actor-lr 1e-4 --critic-lr 1e-4 --train-batch-size 128 --num-samples 5 --actor-delay 2 --timesteps-total 200000 --name nav0-td3-baseline-fcnet-64 --num-cpus-for-driver 2 --num-gpus 1.0 --debug
 
-# nav0-mapo-true-dynamics-fcnet-1024
-mapo --run MAPO --env Navigation-v0 --use-true-dynamics --config-actor-net actor-1024-relu.json --config-critic-net critic-1024-relu.json --actor-lr 1e-4 --critic-lr 1e-4 --sample-batch-size 1 --train-batch-size 2048 --num-samples 5 --name nav0-mapo-true-dynamics-fcnet-1024
+# nav0-mapo-true-dynamics-fcnet-64
+mapo --run MAPO --env Navigation-v0 --use-true-dynamics --config-actor-net fcnet-64-elu.json --config-critic-net fcnet-64-elu.json --actor-lr 1e-4 --critic-lr 1e-4 --train-batch-size 128 --num-samples 5 --timesteps-total 200000 --name nav0-mapo-true-dynamics-fcnet-64 --num-cpus-for-driver 2 --num-gpus 1.0 --debug
 
-# nav0-mapo-mle-fcnet-1024-linear-dynamics
-mapo --run MAPO --env Navigation-v0 --model-loss mle --config-actor-net actor-1024-relu.json --config-critic-net critic-1024-relu.json --config-dynamics-net dynamics-linear-relu.json --actor-lr 1e-4 --critic-lr 1e-4 --dynamics-lr 1e-4 --sample-batch-size 1 --train-batch-size 2048 --num-samples 5 --name nav0-mapo-mle-fcnet-1024-linear-dynamics
+# nav0-mapo-mle-fcnet-64-linear-dynamics
+mapo --run MAPO --env Navigation-v0 --model-loss mle --config-actor-net fcnet-64-elu.json --config-critic-net fcnet-64-elu.json --config-dynamics-net dynamics-linear-relu.json --actor-lr 1e-4 --critic-lr 1e-4 --dynamics-lr 1e-4 --train-batch-size 128 --num-samples 5 --timesteps-total 200000 --name nav0-mapo-mle-fcnet-64-linear-dynamics --num-cpus-for-driver 2 --num-gpus 1.0 --debug
 
-# nav0-mapo-pga-fcnet-1024-linear-dynamics
-mapo --run MAPO --env Navigation-v0 --model-loss pga --config-actor-net actor-1024-relu.json --config-critic-net critic-1024-relu.json --config-dynamics-net dynamics-linear-relu.json --actor-lr 1e-4 --critic-lr 1e-4 --dynamics-lr 1e-4 --sample-batch-size 1 --train-batch-size 2048 --num-samples 5 --name nav0-mapo-pga-fcnet-1024-linear-dynamics
+# nav0-mapo-pga-fcnet-64-linear-dynamics
+mapo --run MAPO --env Navigation-v0 --model-loss pga --config-actor-net fcnet-64-elu.json --config-critic-net fcnet-64-elu.json --config-dynamics-net dynamics-linear-relu.json --actor-lr 1e-4 --critic-lr 1e-4 --dynamics-lr 1e-4 --train-batch-size 128 --num-samples 5 --timesteps-total 200000 --name nav0-mapo-pga-fcnet-64-linear-dynamics --num-cpus-for-driver 2 --num-gpus 1.0 --debug
 
-# nav0-mapo-mle-fcnet-1024
-mapo --run MAPO --env Navigation-v0 --model-loss mle --config-actor-net actor-1024-relu.json --config-critic-net critic-1024-relu.json --config-dynamics-net dynamics-1024-relu.json --actor-lr 1e-4 --critic-lr 1e-4 --dynamics-lr 1e-4 --sample-batch-size 1 --train-batch-size 2048 --num-samples 5 --name nav0-mapo-mle-fcnet-1024
+# nav0-mapo-mle-fcnet-64
+mapo --run MAPO --env Navigation-v0 --model-loss mle --config-actor-net fcnet-64-elu.json --config-critic-net fcnet-64-elu.json --config-dynamics-net fcnet-64-elu.json --actor-lr 1e-4 --critic-lr 1e-4 --dynamics-lr 1e-4 --train-batch-size 128 --num-samples 5 --timesteps-total 200000 --name nav0-mapo-mle-fcnet-64 --num-cpus-for-driver 2 --num-gpus 1.0 --debug
+
+# nav0-mapo-pga-fcnet-64
+mapo --run MAPO --env Navigation-v0 --model-loss pga --config-actor-net fcnet-64-elu.json --config-critic-net fcnet-64-elu.json --config-dynamics-net fcnet-64-elu.json --actor-lr 1e-4 --critic-lr 1e-4 --dynamics-lr 1e-4 --train-batch-size 128 --num-samples 5 --timesteps-total 200000 --name nav0-mapo-pga-fcnet-1024 --num-cpus-for-driver 2 --num-gpus 1.0 --debug
