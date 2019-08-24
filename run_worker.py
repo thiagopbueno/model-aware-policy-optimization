7a312dec2feeafc3832c939055a1290267ec116c
Ângelo Lovatto <angelolovatto@gmail.com>
2019-08-24 10:41:00 -0300

    chore: add script to reproduce layer norm error
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- run_worker.py
+++ run_worker.py
@@ -1,18 +1,32 @@
+# pylint: disable=all
 import mapo
-from mapo.agents.mapo.mapo_policy import MAPOTFPolicy
+from mapo.agents.mapo.mapo_policy import MAPOTFPolicy, get_default_config
 from mapo.envs.registry import ENVS
 
 from ray.rllib.evaluation import RolloutWorker
+from ray.tune.util import merge_dicts
 
 mapo.register_all_agents()
 mapo.register_all_environments()
 
-worker = RolloutWorker(
-    env_creator=ENVS["Navigation-v0"],
-    policy=MAPOTFPolicy,
-    policy_config={"env": "Navigation-v0"},
-)
 
-samples = worker.sample()
-# print("SAMPLES", samples)
-worker.learn_on_batch(samples)
+def main():
+    worker = RolloutWorker(
+        env_creator=ENVS["Navigation-v0"],
+        policy=MAPOTFPolicy,
+        policy_config=merge_dicts(
+            get_default_config(),
+            {
+                "env": "Navigation-v0",
+                "model": {"custom_options": {"actor": {"input_layer_norm": True}}},
+            },
+        ),
+    )
+
+    samples = worker.sample()
+    # print("SAMPLES", samples)
+    worker.learn_on_batch(samples)
+
+
+if __name__ == "__main__":
+    main()
