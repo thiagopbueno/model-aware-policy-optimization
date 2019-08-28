cf49798bfbf0b9801c570fbfc311a64af2d5664d
Ângelo Lovatto <angelolovatto@gmail.com>
2019-08-28 18:11:55 -0300

    feat: use positive reward formulation for Navigation
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- mapo/envs/navigation.py
+++ mapo/envs/navigation.py
@@ -107,7 +107,9 @@ class NavigationEnv(MAPOTFCustomEnv):
     def _reward_fn(self, state, action, next_state):
         # pylint: disable=invalid-unary-operand-type
         goal = tf.constant(self._end, name="goal")
-        return -tf.norm(next_state - goal, axis=-1)
+        distance = -tf.norm(next_state - goal, axis=-1)
+        reward = 100 / (1 + distance)
+        return reward
 
     def _terminal(self):
         reached_goal = np.allclose(self._state, self._end, atol=1e-1)
