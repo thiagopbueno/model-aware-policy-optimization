93674cc45cad21cc6b20bd8730931a0aa3b791c5
Thiago P. Bueno <thiago.pbueno@gmail.com>
2019-08-29 15:02:01 -0300

    feat(envs): add Navigation1DEnv class
    Signed-off-by: Thiago P. Bueno <thiago.pbueno@gmail.com>

--- mapo/envs/registry.py
+++ mapo/envs/registry.py
@@ -17,4 +17,25 @@ def _import_navigation_v1(_):
     return TimeAwareTFEnv(NavigationEnv(**config), horizon=20)
 
 
-ENVS = {"Navigation-v0": _import_navigation_v0, "Navigation-v1": _import_navigation_v1}
+def _import_navigation1d_v0(_):
+    from mapo.envs import TimeAwareTFEnv
+    from mapo.envs.navigation1d import Navigation1DEnv
+
+    config = {}
+    return TimeAwareTFEnv(Navigation1DEnv(**config), horizon=20)
+
+
+def _import_navigation1d_v1(_):
+    from mapo.envs import TimeAwareTFEnv
+    from mapo.envs.navigation1d import Navigation1DEnv
+
+    config = {"n_random_walks": 100}
+    return TimeAwareTFEnv(Navigation1DEnv(**config), horizon=20)
+
+
+ENVS = {
+    "Navigation-v0": _import_navigation_v0,
+    "Navigation-v1": _import_navigation_v1,
+    "Navigation1d-v0": _import_navigation1d_v0,
+    "Navigation1d-v1": _import_navigation1d_v1,
+}
